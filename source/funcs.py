ebfb8b2d4b886f14f8b3678d76cb43675155f967
Ahmed <ali94391@gmail.com>
2021-08-02 14:30:55 +0100

    week 5 finshed

--- source/funcs.py
+++ source/funcs.py
@@ -167,37 +167,70 @@ def delete_courier_from_database(del_courier_id):
     cursor.close()
     connection.close()
 
-# changing stuff
-def change_into_product_db(new_product, new_price, new_product_id):
+
+def change_product_value_in_db(new_product_input, new_price_input, product_id):
     load_dotenv()
     host = os.environ.get("mysql_host")
     user = os.environ.get("mysql_user")
     password = os.environ.get("mysql_pass")
     database = os.environ.get("mysql_db")
-
     connection = pymysql.connect(
         host,
         user,
         password,
         database
     )
-
     cursor = connection.cursor()
-
-    sql = 'UPDATE Products SET Product_Name = %s, Product_Price = %s WHERE Product_Id = %s'
-    val = [(new_product, new_price, new_product_id)]
-    cursor.executemany(sql, val)
-
-    connection.commit()
+    if new_product_input or new_price_input:
+        sql = 'UPDATE products SET'
+        if new_product_input and new_price_input:
+            sql += ' product_name = %s, product_price = %s WHERE product_id = %s'
+            val = (new_product_input, new_price_input, product_id)
+        elif new_product_input:
+            sql += ' product_name = %s WHERE product_id = %s'
+            val = (new_product_input, product_id)
+        elif new_price_input:
+            sql += ' product_price = %s WHERE product_id = %s'
+            val = (new_price_input, product_id)
+        cursor.execute(sql, val)
+        connection.commit()
+    else:
+        print("Nothing has been updated. Entry will stay the same")
     cursor.close()
     connection.close()
     
-# oussamas code
-
-# product_id = int(input('Choose Product ID: '))
-# product_name = input('Choose A Product Name: ')
-# product_price = float(input('Choose A Product Price: '))
-
-# change_into_product_db(product_name, product_price, product_id)
-
-
+    
+def change_courier_value_in_db(new_courier_input, new_courier_num_input, courier_id):
+    load_dotenv()
+    host = os.environ.get("mysql_host")
+    user = os.environ.get("mysql_user")
+    password = os.environ.get("mysql_pass")
+    database = os.environ.get("mysql_db")
+    connection = pymysql.connect(
+        host,
+        user,
+        password,
+        database
+    )
+    cursor = connection.cursor()
+    if new_courier_input or new_courier_num_input:
+        
+        sql = 'UPDATE courier SET'
+        if new_courier_input and new_courier_num_input:
+            sql += ' courier_name = %s, Phone_number = %s WHERE Courier_id = %s'
+            val = (new_courier_input, new_courier_num_input, courier_id)
+            
+        elif new_courier_input:
+            sql += ' courier_name = %s WHERE Courier_id = %s'
+            val = (new_courier_input, courier_id)
+            print('\ncourier number has not been changed')
+        elif new_courier_num_input:
+            sql += ' Phone_number = %s WHERE Courier_id = %s'
+            val = (new_courier_num_input, courier_id)
+            print('\ncourier name has not been changed')
+        cursor.execute(sql, val)
+        connection.commit()
+    else:
+        print("Nothing has been updated. Entry will stay the same")
+    cursor.close()
+    connection.close()
